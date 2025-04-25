83d3e651c4c84a9b3f5405a0cfe119eef89f1b54
Titus Silver <titus.silver.ph@gmail.com>
2025-04-25 21:09:08 +0800

    update code

---
+++ Form1.Designer.vb
@@ -0,0 +1,73 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        components = New ComponentModel.Container()
+        PictureBox1 = New PictureBox()
+        Timer1 = New Timer(components)
+        btnStartDetection = New Button()
+        CType(PictureBox1, ComponentModel.ISupportInitialize).BeginInit()
+        SuspendLayout()
+        ' 
+        ' PictureBox1
+        ' 
+        PictureBox1.BackColor = SystemColors.AppWorkspace
+        PictureBox1.Location = New Point(12, 12)
+        PictureBox1.Name = "PictureBox1"
+        PictureBox1.Size = New Size(600, 317)
+        PictureBox1.SizeMode = PictureBoxSizeMode.Zoom
+        PictureBox1.TabIndex = 0
+        PictureBox1.TabStop = False
+        ' 
+        ' Timer1
+        ' 
+        ' 
+        ' btnStartDetection
+        ' 
+        btnStartDetection.FlatStyle = FlatStyle.Flat
+        btnStartDetection.Font = New Font("Impact", 15.75F, FontStyle.Regular, GraphicsUnit.Point)
+        btnStartDetection.Location = New Point(192, 345)
+        btnStartDetection.Name = "btnStartDetection"
+        btnStartDetection.Size = New Size(198, 58)
+        btnStartDetection.TabIndex = 1
+        btnStartDetection.Text = "Start Detection"
+        btnStartDetection.UseVisualStyleBackColor = True
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(7F, 15F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(624, 450)
+        Controls.Add(btnStartDetection)
+        Controls.Add(PictureBox1)
+        Name = "Form1"
+        StartPosition = FormStartPosition.CenterScreen
+        Text = "Form1"
+        CType(PictureBox1, ComponentModel.ISupportInitialize).EndInit()
+        ResumeLayout(False)
+    End Sub
+
+    Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents Timer1 As Timer
+    Friend WithEvents btnStartDetection As Button
+End Class
---
+++ Form1.vb
@@ -0,0 +1,96 @@
+﻿Imports System.IO
+Imports System.Net.Http
+
+Public Class Form1
+    Private WithEvents _httpClient As New HttpClient()
+    Private _stream As Stream = Nothing
+    Private _buffer As Byte() = New Byte(4096) {}
+    Private _imageBuffer As New MemoryStream()
+    Private _image As Bitmap
+    Private _isInFrame As Boolean = False
+    Private _isDetectionRunning As Boolean = False ' Flag to check if detection is running
+
+
+    Private Async Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Timer1.Interval = 100
+        Timer1.Start()
+
+        Try
+            ' Connect to the Flask video stream
+            Dim streamResponse As Stream = Await _httpClient.GetStreamAsync("http://localhost:5000/video_feed")
+            _stream = streamResponse
+            Console.WriteLine("Connected to video stream.")
+        Catch ex As Exception
+            MessageBox.Show("Error connecting to video stream: " & ex.Message)
+            Console.WriteLine("Error connecting to video stream: " & ex.Message)
+        End Try
+    End Sub
+
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+        If _stream IsNot Nothing Then
+            Try
+                ' Read data from the stream
+                Dim bytesRead As Integer = _stream.Read(_buffer, 0, _buffer.Length)
+
+                If bytesRead > 0 Then
+                    ' Process the data by checking for start and end of JPEG frames
+                    For i As Integer = 0 To bytesRead - 1
+                        If _isInFrame Then
+                            _imageBuffer.WriteByte(_buffer(i))
+
+                            ' Check for end of JPEG frame (0xFF 0xD9 is the JPEG end marker)
+                            If _buffer(i) = &HFF AndAlso i + 1 < bytesRead AndAlso _buffer(i + 1) = &HD9 Then
+                                ' End of the frame found
+                                _isInFrame = False
+
+                                ' Process the complete frame
+                                _imageBuffer.Seek(0, SeekOrigin.Begin)
+                                _image = New Bitmap(_imageBuffer)
+                                PictureBox1.Image = _image
+                                _imageBuffer.SetLength(0) ' Reset the buffer for the next frame
+                                Console.WriteLine("Frame displayed.")
+                            End If
+                        Else
+                            ' Look for the start of a new JPEG frame (0xFF 0xD8 is the JPEG start marker)
+                            If _buffer(i) = &HFF AndAlso i + 1 < bytesRead AndAlso _buffer(i + 1) = &HD8 Then
+                                _isInFrame = True
+                                _imageBuffer.SetLength(0) ' Reset buffer to start collecting new frame
+                                _imageBuffer.WriteByte(_buffer(i))
+                                _imageBuffer.WriteByte(_buffer(i + 1))
+                                i += 1 ' Skip the next byte since it's part of the start marker
+                            End If
+                        End If
+                    Next
+                End If
+            Catch ex As Exception
+                MessageBox.Show("Error reading from stream: " & ex.Message)
+                Console.WriteLine("Error reading from stream: " & ex.Message)
+            End Try
+        End If
+    End Sub
+
+    Private Sub Form1_FormClosing(sender As Object, e As FormClosingEventArgs) Handles MyBase.FormClosing
+        Timer1.Stop()
+        _httpClient.Dispose()
+        _imageBuffer.Dispose()
+        _stream.Dispose()
+    End Sub
+
+    Private Async Sub btnStartDetection_Click(sender As Object, e As EventArgs) Handles btnStartDetection.Click
+        If Not _isDetectionRunning Then
+            _isDetectionRunning = True
+            Timer1.Start() ' Start the timer when detection is activated
+            Try
+                ' Connect to the Flask video stream
+                Dim streamResponse As Stream = Await _httpClient.GetStreamAsync("http://localhost:5000/video_feed")
+                _stream = streamResponse
+                Console.WriteLine("Connected to video stream.")
+            Catch ex As Exception
+                MessageBox.Show("Error connecting to video stream: " & ex.Message)
+                Console.WriteLine("Error connecting to video stream: " & ex.Message)
+            End Try
+        Else
+            MessageBox.Show("Detection is already running.")
+        End If
+    End Sub
+End Class
